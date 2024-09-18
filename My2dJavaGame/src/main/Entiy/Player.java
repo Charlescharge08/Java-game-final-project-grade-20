a04afe748914dacb63deaceb10aa82783fd5afd4
James Peterson <jpetes08@outlook.com>
2024-09-18 11:51:53 -0600

    with sprite for caricter

--- My2dJavaGame/src/main/Entiy/Player.java
+++ My2dJavaGame/src/main/Entiy/Player.java
@@ -1,7 +1,8 @@
 package Entiy;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 import main.GamePanel;
 import main.keyHandler;
 
@@ -13,28 +14,73 @@ public class Player extends entiy{
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
     }
+
+    public void getPlayerImage(){
+        try {
+            
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+
     public void setDefaultValues(){
         x = 100;
         y = 100;
         speed = 2;
+        direction = "down";
     } 
     public void update(){
         if(keyH.upPressed){
             y -= speed;
+            direction = "up";
         }
         if(keyH.downPressed){
             y += speed;
+            direction = "down";
         }
         if(keyH.leftPressed){
             x -= speed;
+            direction = "left";
         }
         if(keyH.rightPressed){
             x += speed;
+            direction = "right";
         }
     }
     public void draw(Graphics2D g2){
-        g2.setColor(Color.WHITE);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+//       g2.setColor(Color.WHITE);
+//       g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+        
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    
     }
 }
--- My2dJavaGame/src/main/Entiy/entiy.java
+++ My2dJavaGame/src/main/Entiy/entiy.java
@@ -1,6 +1,11 @@
 package Entiy;
 
+import java.awt.image.BufferedImage;
+
 public class entiy {
     public int x,y;
     public int speed;
+
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
 }
