eafde603c20db9dac4450d5ec26838af1d8781f9
James Peterson <jpetes08@outlook.com>
2024-09-18 09:47:36 -0600

    Second game loop option Delta (Has FPS display)

--- My2dJavaGame/src/main/main/GamePanel.java
+++ My2dJavaGame/src/main/main/GamePanel.java
@@ -43,35 +43,66 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread.start();
     }
 
-    @Override
-    public void run() {
+//    @Override
+//   public void run() {
 
-        double drawInterval = 1000000000 / FPS;
-        double nextDrawTime = System.nanoTime() + drawInterval;
+//       double drawInterval = 1000000000 / FPS;
+//       double nextDrawTime = System.nanoTime() + drawInterval;
 
-        while (gameThread != null) {
+//      while (gameThread != null) {
 
             // 1 Update: update information such as player position, enemy position, etc.
-            update();
+//           update();
             // 2 Draw: draw the screen with the updated information.
-            repaint();
+//           repaint();
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime / 1000000; // convert to milliseconds for the sleep method
+//           try {
+//               double remainingTime = nextDrawTime - System.nanoTime();
+//              remainingTime = remainingTime / 1000000; // convert to milliseconds for the sleep method
 
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
+//              if (remainingTime < 0) {
+//                   remainingTime = 0;
+//               }
 
-                Thread.sleep((long) remainingTime);
-                nextDrawTime += drawInterval;
+//                Thread.sleep((long) remainingTime);
+//                nextDrawTime += drawInterval;
 
-            } catch (InterruptedException e) {
+//            } catch (InterruptedException e) {
 
-                e.printStackTrace();
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    public void run(){
+        
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
             }
         }
+        
     }
 
     public void update() {
